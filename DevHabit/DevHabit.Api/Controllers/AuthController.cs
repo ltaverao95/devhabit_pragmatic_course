0072885a84ce7c9256028dffa7230bb5398a13a5
luistaveraflyr <luis.tavera@newshore.es>
2026-02-12 17:13:23 -0500

    Add JWT authentication and secure API endpoints
    Implemented JWT-based authentication:
    - Added TokenProvider service and related DTOs
    - Created JwtAuthOptions for JWT config
    - Updated AuthController for token issuance on register/login
    - Secured controllers with [Authorize] attribute
    - Configured authentication/authorization middleware
    - Added JwtBearer NuGet package and JWT settings

--- DevHabit/DevHabit.Api/Controllers/AuthController.cs
+++ DevHabit/DevHabit.Api/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using DevHabit.Api.DTOs.Auth;
 using DevHabit.Api.DTOs.Users;
 using DevHabit.Api.Entities;
+using DevHabit.Api.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -15,10 +16,11 @@ namespace DevHabit.Api.Controllers;
 [AllowAnonymous]
 public sealed class AuthController(UserManager<IdentityUser> userManager,
                                    ApplicationIdentityDbContext identityDbContext,
-                                   ApplicationDbContext applicationDbContext) : ControllerBase
+                                   ApplicationDbContext applicationDbContext,
+                                   TokenProvider tokenProvider) : ControllerBase
 {
     [HttpPost("register")]
-    public async Task<IActionResult> Register(RegisterUserDto registerUserDto)
+    public async Task<ActionResult<AccessTokensDto>> Register(RegisterUserDto registerUserDto)
     {
         using IDbContextTransaction transaction = await identityDbContext.Database.BeginTransactionAsync();
         applicationDbContext.Database.SetDbConnection(identityDbContext.Database.GetDbConnection());
@@ -56,6 +58,34 @@ public sealed class AuthController(UserManager<IdentityUser> userManager,
 
         await transaction.CommitAsync();
 
-        return Ok(user.Id);
+        var tokenRequest = new TokenRequest(identityUser.Id, identityUser.Email);
+        AccessTokensDto accessTokens = tokenProvider.Create(tokenRequest);
+
+        return Ok(accessTokens);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AccessTokensDto>> Login(LoginUserDto loginUserDto)
+    {
+        IdentityUser? identityUser = await userManager.FindByEmailAsync(loginUserDto.Email);
+        if (identityUser is null)
+        {
+            return Problem(
+                detail: "Invalid email or password",
+                statusCode: StatusCodes.Status401Unauthorized);
+        }
+
+        var isPasswordValid = await userManager.CheckPasswordAsync(identityUser, loginUserDto.Password);
+        if (!isPasswordValid)
+        {
+            return Problem(
+                detail: "Invalid email or password",
+                statusCode: StatusCodes.Status401Unauthorized);
+        }
+
+        var tokenRequest = new TokenRequest(identityUser.Id, identityUser.Email!);
+        AccessTokensDto accessTokens = tokenProvider.Create(tokenRequest);
+
+        return Ok(accessTokens);
     }
 }
